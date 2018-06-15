6f7d44c71de51ff8bf43d235380e30880583ba50
goodluck2018 <1057357150@qq.com>
2018-06-15 10:30:58 +0800

    完成个人中心页面

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['pages/logs/main', '^pages/index/main'],
+    pages: ['pages/logs/main', 'pages/index/main', '^pages/personal/main'],
     tabBar: {
       backgroundColor: '#fff',
       list: [{
@@ -23,7 +23,7 @@ export default {
         iconPath: '/static/img/community.png',
         text: '铲屎官社区'
       }, {
-        pagePath: 'pages/test/main',
+        pagePath: 'pages/personal/main',
         iconPath: '/static/img/self-center.png',
         text: '个人中心'
       }]

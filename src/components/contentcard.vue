fdde5556ba49e33469d67143a05274f7df3906f0
goodluck2018 <1057357150@qq.com>
2018-06-21 00:22:41 +0800

    '个人中心增加mock模拟，完成组件遍历渲染，同时更新组件点击事件'

--- src/components/contentcard.vue
+++ src/components/contentcard.vue
@@ -3,50 +3,41 @@
     <!-- 此处将通过props从mytimeline.vue父组件接受数据 -->
     <!-- 然后在li中进行v-for遍历渲染DOM -->
     <ul>
-      <li class="list-wrapper" @click="toggleRoundHint">
+      <li v-for="(comment, index_comment) in commentdata.myTimeLine" :key="index_comment" class="list-wrapper" @click="toggleRoundHint">
         <!-- 时间轴上的圆点 -->
         <div class="round-outter">
           <div class="round-inner" :class="showRound ? 'active' : ''"></div>
         </div>
         <!-- 日期区 -->
-        <p class="date">5月2日 - 下午12:37</p>
+        <p class="date">{{comment.date}}</p>
         <p class="content">
-          我是傲娇的铲屎官
+          {{comment.content}}
         </p>
         <!-- 图片展示区 / 此处将从后端获取数据后通过v-for渲染 -->
         <scroll-view class="scroll-view" scroll-x>
           <ul class="img-list">
-            <li class="img-wrapper">
-              <img src="../../static/img/timeline/day1-1.jpg" mode="aspectFill"/>
-            </li>
-            <li class="img-wrapper">
-              <img src="../../static/img/timeline/day1-2.png" mode="aspectFill"/>
-            </li>
-            <li class="img-wrapper">
-              <img src="../../static/img/timeline/day1-3.jpg" mode="aspectFill"/>
-            </li>
-            <li class="img-wrapper">
-              <img src="../../static/img/timeline/day1-4.jpg" mode="aspectFill"/>
+            <li v-for="(img, index_img) in comment.imgs" :key="index_img" class="img-wrapper">
+              <img :src="img" mode="aspectFill"/>
             </li>
           </ul>
         </scroll-view>
         <!-- 赞+评论+分享组件 -->
         <div class="tools">
-          <div class="obox like-btn" @click.stop="toggleLike">
-            <span class="icon" :class="isLike ? 'icon-like_active' : 'icon-like'"></span>
-            <span class="txt" :class="isLike ? 'a-color' : ''">赞</span>
+          <div class="obox like-btn" @click.stop="toggleLike(comment)">
+            <span class="icon" :class="comment.like ? 'icon-like_active' : 'icon-like'"></span>
+            <span class="txt" :class="comment.like ? 'a-color' : ''">赞</span>
           </div>
-          <div class="obox comment-btn" @click.stop="toggleComment">
-            <span class="icon icon-comment" :class="showComment ? 'icon-comment_active' : 'icon-comment'"></span>
-            <span class="txt" :class="showComment ? 'a-color' : ''">评论</span>
+          <div class="obox comment-btn" @click.stop="toggleComment(comment)">
+            <span class="icon icon-comment" :class="comment.showComment ? 'icon-comment_active' : 'icon-comment'"></span>
+            <span class="txt" :class="comment.showComment ? 'a-color' : ''">评论</span>
           </div>
           <!-- 分享组件 -->
           <div class="obox share-btn">
-            <div @click.stop="toggleShare">
-              <span class="icon" :class="showShare ? 'icon-share_active' : 'icon-share'"></span>
-              <span class="txt" :class="showShare ? 'a-color' : ''">分享</span>
+            <div @click.stop="toggleShare(comment)">
+              <span class="icon" :class="comment.showShare ? 'icon-share_active' : 'icon-share'"></span>
+              <span class="txt" :class="comment.showShare ? 'a-color' : ''">分享</span>
             </div>
-            <div class="share-group" v-show="showShare">
+            <div class="share-group" v-show="comment.showShare">
               <div class="wrapper">
                 <span class="icon icon-weixin"></span>
                 <span>微信朋友</span>
@@ -64,192 +55,16 @@
           </div>
         </div>
         <!-- 评论内容 -->
-        <div class="comment-content" v-show="showComment">
+        <div class="comment-content" v-show="comment.showComment">
           <ul class="list">
-            <li class="item">
-              <span class="user">堃：</span>
-              <span class="txt">用颜色表示性别是可以的啊比如说粉色代表女，蓝色代表男，但是背景颜色需要改一下。不然看不清楚了。</span>
-            </li>
-            <li class="item">
-              <span class="user">老菜鸡：</span>
-              <span class="txt">刚画完我就后悔了</span>
-            </li>
-            <li class="item">
-              <span class="user">老菜鸡：</span>
-              <span class="txt">不然用标志吧，文字有颜色感觉太花了</span>
-            </li>
-            <li class="item">
-              <span class="user">一个梨子：</span>
-              <span class="txt">可以用黑白表示性别吗？</span>
+            <li v-for="(content, index_content) in comment.comments" :key="index_content" class="item">
+              <span class="user">{{content.userName}}：</span>
+              <span class="txt">{{content.content}}</span>
             </li>
           </ul>
           <form>
             <div class="reply">
-              <img class="avatar" src=""/>
-              <input class="input" placeholder="评论"/>
-              <button class="submit">发布</button>
-            </div>
-          </form>
-        </div>
-      </li>
-      <!-- 下面2个li是模拟遍历后的结果 -->
-      <li class="list-wrapper" @click="toggleRoundHint">
-        <!-- 时间轴上的圆点 -->
-        <div class="round-outter">
-          <div class="round-inner" :class="showRound ? 'active' : ''"></div>
-        </div>
-        <!-- 日期区 -->
-        <p class="date">5月2日 - 下午12:37</p>
-        <p class="content">
-          今天老子心情不是很好，别惹我
-        </p>
-        <!-- 图片展示区 / 此处将从后端获取数据后通过v-for渲染 -->
-        <scroll-view class="scroll-view" scroll-x>
-          <ul class="img-list">
-            <li class="img-wrapper">
-              <img src="../../static/img/timeline/day2.jpg" mode="aspectFill"/>
-            </li>
-          </ul>
-        </scroll-view>
-        <!-- 赞+评论+分享组件 -->
-        <div class="tools">
-          <div class="obox like-btn" @click.stop="toggleLike">
-            <span class="icon" :class="isLike ? 'icon-like_active' : 'icon-like'"></span>
-            <span class="txt" :class="isLike ? 'a-color' : ''">赞</span>
-          </div>
-          <div class="obox comment-btn" @click.stop="toggleComment">
-            <span class="icon icon-comment" :class="showComment ? 'icon-comment_active' : 'icon-comment'"></span>
-            <span class="txt" :class="showComment ? 'a-color' : ''">评论</span>
-          </div>
-          <!-- 分享组件 -->
-          <div class="obox share-btn">
-            <div @click.stop="toggleShare">
-              <span class="icon" :class="showShare ? 'icon-share_active' : 'icon-share'"></span>
-              <span class="txt" :class="showShare ? 'a-color' : ''">分享</span>
-            </div>
-            <div class="share-group" v-show="showShare">
-              <div class="wrapper">
-                <span class="icon icon-weixin"></span>
-                <span>微信朋友</span>
-              </div>
-              <div class="wrapper">
-              <span class="icon icon-pengyouquan"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span></span>
-              <span>朋友圈</span>
-              </div>
-              <div class="wrapper">
-                <span class="icon icon-copylink"></span>
-                <span>复制链接</span>
-              </div>
-              <div class="dt"></div>
-            </div>
-          </div>
-        </div>
-        <!-- 评论内容 -->
-        <div class="comment-content" v-show="showComment">
-          <ul class="list">
-            <li class="item">
-              <span class="user">堃：</span>
-              <span class="txt">用颜色表示性别是可以的啊比如说粉色代表女，蓝色代表男，但是背景颜色需要改一下。不然看不清楚了。</span>
-            </li>
-            <li class="item">
-              <span class="user">老菜鸡：</span>
-              <span class="txt">刚画完我就后悔了</span>
-            </li>
-            <li class="item">
-              <span class="user">老菜鸡：</span>
-              <span class="txt">不然用标志吧，文字有颜色感觉太花了</span>
-            </li>
-            <li class="item">
-              <span class="user">一个梨子：</span>
-              <span class="txt">可以用黑白表示性别吗？</span>
-            </li>
-          </ul>
-          <form>
-            <div class="reply">
-              <img class="avatar" src=""/>
-              <input class="input" placeholder="评论"/>
-              <button class="submit">发布</button>
-            </div>
-          </form>
-        </div>
-      </li>
-      <li class="list-wrapper" @click="toggleRoundHint">
-        <!-- 时间轴上的圆点 -->
-        <div class="round-outter">
-          <div class="round-inner" :class="showRound ? 'active' : ''"></div>
-        </div>
-        <!-- 日期区 -->
-        <p class="date">5月2日 - 下午12:37</p>
-        <p class="content">
-          每位铲屎官都有一颗萌萌的心
-        </p>
-        <!-- 图片展示区 / 此处将从后端获取数据后通过v-for渲染 -->
-        <scroll-view class="scroll-view" scroll-x>
-          <ul class="img-list">
-            <li class="img-wrapper">
-              <img src="../../static/img/timeline/day3-1.jpg" mode="aspectFill"/>
-            </li>
-            <li class="img-wrapper">
-              <img src="../../static/img/timeline/day3-2.jpg" mode="aspectFill"/>
-            </li>
-          </ul>
-        </scroll-view>
-        <!-- 赞+评论+分享组件 -->
-        <div class="tools">
-          <div class="obox like-btn" @click.stop="toggleLike">
-            <span class="icon" :class="isLike ? 'icon-like_active' : 'icon-like'"></span>
-            <span class="txt" :class="isLike ? 'a-color' : ''">赞</span>
-          </div>
-          <div class="obox comment-btn" @click.stop="toggleComment">
-            <span class="icon icon-comment" :class="showComment ? 'icon-comment_active' : 'icon-comment'"></span>
-            <span class="txt" :class="showComment ? 'a-color' : ''">评论</span>
-          </div>
-          <!-- 分享组件 -->
-          <div class="obox share-btn">
-            <div @click.stop="toggleShare">
-              <span class="icon" :class="showShare ? 'icon-share_active' : 'icon-share'"></span>
-              <span class="txt" :class="showShare ? 'a-color' : ''">分享</span>
-            </div>
-            <div class="share-group" v-show="showShare">
-              <div class="wrapper">
-                <span class="icon icon-weixin"></span>
-                <span>微信朋友</span>
-              </div>
-              <div class="wrapper">
-              <span class="icon icon-pengyouquan"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span></span>
-              <span>朋友圈</span>
-              </div>
-              <div class="wrapper">
-                <span class="icon icon-copylink"></span>
-                <span>复制链接</span>
-              </div>
-              <div class="dt"></div>
-            </div>
-          </div>
-        </div>
-        <!-- 评论内容 -->
-        <div class="comment-content" v-show="showComment">
-          <ul class="list">
-            <li class="item">
-              <span class="user">堃：</span>
-              <span class="txt">用颜色表示性别是可以的啊比如说粉色代表女，蓝色代表男，但是背景颜色需要改一下。不然看不清楚了。</span>
-            </li>
-            <li class="item">
-              <span class="user">老菜鸡：</span>
-              <span class="txt">刚画完我就后悔了</span>
-            </li>
-            <li class="item">
-              <span class="user">老菜鸡：</span>
-              <span class="txt">不然用标志吧，文字有颜色感觉太花了</span>
-            </li>
-            <li class="item">
-              <span class="user">一个梨子：</span>
-              <span class="txt">可以用黑白表示性别吗？</span>
-            </li>
-          </ul>
-          <form>
-            <div class="reply">
-              <img class="avatar" src=""/>
+              <img class="avatar" :src="commentdata.avatarLink"/>
               <input class="input" placeholder="评论"/>
               <button class="submit">发布</button>
             </div>
@@ -261,24 +76,30 @@
 </template>
 
 <script>
+import Vue from 'vue'
 export default {
-  data () {
-    return {
-      isLike: false,
-      showComment: false,
-      showShare: false,
-      showRound: false
+  props: {
+    commentdata: {
+      type: Object
     }
   },
   methods: {
-    toggleLike () {
-      this.isLike = !this.isLike
+    toggleLike (comment) {
+      comment.like = !comment.like
     },
-    toggleComment () {
-      this.showComment = !this.showComment
+    toggleComment (comment) {
+      if (!comment.showComment) {
+        Vue.set(comment, 'showComment', true)
+      } else {
+        comment.showComment = !comment.showComment
+      }
     },
-    toggleShare () {
-      this.showShare = !this.showShare
+    toggleShare (comment) {
+      if (!comment.showShare) {
+        Vue.set(comment, 'showShare', true)
+      } else {
+        comment.showShare = !comment.showShare
+      }
     },
     toggleRoundHint () {
       this.showRound = !this.showRound

53a130cb899f8050624eda6d08df24bf50c20061
Zhuyuqi <396838319@qq.com>
2018-06-26 13:55:45 +0800

    添加热门卡片组件

---
+++ src/components/hotCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hotCard">
+    <img :src="src" alt="">
+    <div class="content">
+      <p>{{name}}</p>
+      <p>{{date}}</p>
+      <p>{{host}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: '1'
+    },
+    date: {
+      type: String,
+      default: '2'
+    },
+    host: {
+      type: String,
+      default: '3'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hotCard {
+  width: 100%;
+  height: 500rpx;
+  position: relative;
+  img {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+  }
+  .content {
+    width: 100%;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    transform: translate(0, -50%);
+    left: 0;
+    color: #fff;
+    font-family: 'Courier New', Courier, monospace;
+  }
+}
+</style>
--- src/pages/test1/index.vue
+++ src/pages/test1/index.vue
@@ -1,38 +1,37 @@
 <template>
     <div class="time-card">
       <div class="img-card" v-for="item in imgList" :key="item.catName">
-        <div class="cards">
-          <h6>{{item.catName}}</h6>
-          <p>{{item.time}}</p>
-          <p>{{item.catHost}}</p>
-        </div>
-        <img :src="item.img" alt="">
+        <hot-card :name="item.catName" :date="item.date" :host="item.catHost" :src="item.img"></hot-card>
       </div>
     </div>
 </template>
 
 <script>
+import hotCard from '@/components/hotCard'
 export default {
+  components: {
+    hotCard: hotCard
+  },
   data () {
     return {
       imgList: [
         {
           catName: '咖啡猫',
-          time: '2018.05.01',
+          date: '2018.05.01',
           catHost: '果果',
-          img: '../../../static/img/timeline/day3-2.jpg'
+          img: '../../../static/img/timeline/day1-1.jpg'
         },
         {
           catName: '咖啡猫',
-          time: '2018.05.01',
+          date: '2018.05.01',
           catHost: '果果',
-          img: '../../../static/img/timeline/day3-2.jpg'
+          img: '../../../static/img/timeline/day1-2.png'
         },
         {
           catName: '咖啡猫',
-          time: '2018.05.01',
+          date: '2018.05.01',
           catHost: '果果',
-          img: '../../../static/img/timeline/day3-2.jpg'
+          img: '../../../static/img/timeline/day1-3.jpg'
         }
       ]
     }
@@ -47,23 +46,12 @@ export default {
   @import '../../../static/font/iconfont.css'
   .time-card
     width: 100%
+    overflow: hidden
     .img-card
-      padding: 20px
+      padding: 10px
+      overflow: hidden
       width: 90%
       height: 240px
+      margin: 0 auto;
       position: relative
-      .cards
-        position: absolute
-        top: 120px
-        left: 150px
-        color: #fff
-        h6
-          text-align: center
-        p
-          text-align: center
-          font-size: 10px
-      img
-        width: 335px
-        height: 240px
-
   </style>
